af2b3c4bf460938952b75b51328d5cd8e2206902
Michele Manco <87037954+MicheleManco@users.noreply.github.com>
2021-11-15 13:15:52 +0100

    impostazione lato dx

---
+++ script.js
@@ -0,0 +1,119 @@
+
+var myapp = new Vue({
+    el: "#myapp",
+data:{
+    contacts: [
+        {
+            name: ' Michele',
+            avatar: ' _1',
+            visible: true,
+            messages: [
+                {
+                date: ' 10/01/2020 1 5:30:55',
+                text: ' Hai p ortato a s passo i l c ane?',
+                status: ' sent'
+                },
+                {
+                date: ' 10/01/2020 1 5:50:00',
+                text: ' Ricordati d i d argli d a m angiare',
+                status: ' sent'
+                },
+                {
+                date: ' 10/01/2020 1 6:15:22',
+                text: ' Tutto f atto!',
+                status: ' received'
+                }
+            ],
+        },
+        {
+            name: ' Michele',
+            avatar: ' _1',
+            visible: true,
+            messages: [
+                {
+                date: ' 10/01/2020 1 5:30:55',
+                text: ' Hai p ortato a s passo i l c ane?',
+                status: ' sent'
+                },
+                {
+                date: ' 10/01/2020 1 5:50:00',
+                text: ' Ricordati d i d argli d a m angiare',
+                status: ' sent'
+                },
+                {
+                date: ' 10/01/2020 1 6:15:22',
+                text: ' Tutto f atto!',
+                status: ' received'
+                }
+            ],
+        },
+        {
+            name: ' Michele',
+            avatar: ' _1',
+            visible: true,
+            messages: [
+                {
+                date: ' 10/01/2020 1 5:30:55',
+                text: ' Hai p ortato a s passo i l c ane?',
+                status: ' sent'
+                },
+                {
+                date: ' 10/01/2020 1 5:50:00',
+                text: ' Ricordati d i d argli d a m angiare',
+                status: ' sent'
+                },
+                {
+                date: ' 10/01/2020 1 6:15:22',
+                text: ' Tutto f atto!',
+                status: ' received'
+                }
+            ],
+        },
+        {
+            name: ' Michele',
+            avatar: ' _1',
+            visible: true,
+            messages: [
+                {
+                date: ' 10/01/2020 1 5:30:55',
+                text: ' Hai p ortato a s passo i l c ane?',
+                status: ' sent'
+                },
+                {
+                date: ' 10/01/2020 1 5:50:00',
+                text: ' Ricordati d i d argli d a m angiare',
+                status: ' sent'
+                },
+                {
+                date: ' 10/01/2020 1 6:15:22',
+                text: ' Tutto f atto!',
+                status: ' received'
+                }
+            ],
+        },
+        {
+            name: ' Michele',
+            avatar: ' _1',
+            visible: true,
+            messages: [
+                {
+                date: ' 10/01/2020 1 5:30:55',
+                text: ' Hai p ortato a s passo i l c ane?',
+                status: ' sent'
+                },
+                {
+                date: ' 10/01/2020 1 5:50:00',
+                text: ' Ricordati d i d argli d a m angiare',
+                status: ' sent'
+                },
+                {
+                date: ' 10/01/2020 1 6:15:22',
+                text: ' Tutto f atto!',
+                status: ' received'
+                }
+            ],
+        },
+       
+    ]
+}
+
